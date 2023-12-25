4967a53bbcbb7cb30d4e35c390d0481d274dcdfe
Gabriel Rocha <g.rocha.1997@gmail.com>
2023-12-25 10:05:17 -0300

    exercise 05 to 08

---
+++ 1 - Fixed-Size Data/08 - tall-or-wide.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |08 - tall-or-wide|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "testing.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "testing.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define cat (bitmap "img/cat.png"))
+(define HEIGHT (image-height cat))
+(define WIDTH (image-width cat))
+
+(if (>= HEIGHT WIDTH) "tall" "wide")
+
+(cond
+  [(> HEIGHT WIDTH) "tail"]
+  [(< HEIGHT WIDTH) "wide"]
+  [else "square"])
